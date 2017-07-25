69ba4a5fbf1642f3f6a81106d22d5e4b0c42656d
Cliff Earl <cearl75@gmail.com>
2017-07-25 21:14:13 +1200

    Create TextureCache.lua

---
+++ TextureCache.lua
@@ -0,0 +1,32 @@
+
+TextureCache = Core.class()
+
+function TextureCache:init()
+  self.textures = {}
+end
+
+function TextureCache:reset() -- flush all loaded textures
+  self.textures = {}
+  collectgarbage("collect")
+end
+
+function TextureCache:removeTexture(name) -- uncache named texture
+  local textures = self.textures
+  textures[name] = nil
+  collectgarbage("collect")
+end
+
+function TextureCache:getTexture(name, filter) -- returns cached or new texture
+  filter = filter or false
+  local textures = self.textures
+  local texture
+  if textures[name] then
+    texture = textures[name] -- return cached texture
+    print("returning cached texture " .. name)
+  else
+    texture = Texture.new(name, filter) -- create and return new texture
+    textures[name] = texture
+--    print("cached new texture " .. name)
+  end
+  return texture
+end
